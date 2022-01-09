227bde17ff050b2a4f2a4f417bc1e578e2933f12
george <georgegabriel93@hotmail.com>
2022-01-09 20:47:53 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Item from './components/Item';
+import Card from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <>
+    <h1>Minha primeira aplicação com React</h1>
+    <ul>
+      <Item texto='Item 1'/> 
+      <Item texto='Item 2'/>
+      <Item texto='Item 3'/>
+    </ul>
+    <Card />
+    </>
+
+  )
 }
 
 export default App;
